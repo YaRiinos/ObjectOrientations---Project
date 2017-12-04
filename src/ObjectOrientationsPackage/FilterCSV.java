5d023c989a9a633fcf8b31c93d7c07af5ff6f762
Yarin Knafo <yarin665@gmail.com>
2017-12-04 21:34:46 +0200

    Add radius to location filter
    Add distFrom function to calculate the distance between 2 points

--- src/ObjectOrientationsPackage/FilterCSV.java
+++ src/ObjectOrientationsPackage/FilterCSV.java
@@ -84,14 +84,19 @@ public class FilterCSV {
 
 			//Get location
 			else if(input.equals("3")) {
+				double radius;
 				System.out.println("Type the exact Location: (Format:Lat,Lon)");
 				locationInput= scanner.nextLine();
+				System.out.println("Enter a valid radius: ");
+				radius=scanner.nextDouble();
 				try (BufferedReader br = new BufferedReader(new FileReader(firstLocation))){
 					br.readLine(); // this will read the first line
 					while ((line = br.readLine()) != null) {
 						String[] column = line.split(cvsSplitBy);
 						String[] locationSplit=locationInput.split(",");
-						if(column[2].equals(locationSplit[0])&&column[3].equals(locationSplit[1])) {
+						double dis=distFrom(Double.valueOf(locationSplit[0]), Double.valueOf(locationSplit[1]),
+								Double.valueOf(column[2]), Double.valueOf(column[3]));
+						if(dis<=radius){
 							thePrint(column, firstLocation, lastLocation, counter);
 							counter++;
 
@@ -230,5 +235,19 @@ public class FilterCSV {
 	public void setLastLocation(String lastLocation) {
 		this.lastLocation = lastLocation;
 	}
+	
+	public static double distFrom(double lat1, double lng1, double lat2, double lng2) {
+	    double earthRadius = 3958.75; // miles (or 6371.0 kilometers)
+	    double dLat = Math.toRadians(lat2-lat1);
+	    double dLng = Math.toRadians(lng2-lng1);
+	    double sindLat = Math.sin(dLat / 2);
+	    double sindLng = Math.sin(dLng / 2);
+	    double a = Math.pow(sindLat, 2) + Math.pow(sindLng, 2)
+	            * Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2));
+	    double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+	    double dist = earthRadius * c;
+
+	    return dist;
+	    }
 	
 }
