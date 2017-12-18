dfec8b1953e379f2735f8f3cd5f1bda53b482028
yakirarie <33618271+yakirarie@users.noreply.github.com>
2017-12-18 16:06:42 +0200

    packaged codes

---
+++ src/CSV/MergeCSV.java
@@ -0,0 +1,39 @@
+package CSV;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MergeCSV {
+
+	
+	/**
+	 * This function get all the new CSV (after converting them according to the format)
+	 * and merge them to one file
+	 * @param paths The list where all file locations are stored 
+	 * @return The new String list containing all the info
+	 * @throws IOException
+	 */
+	public static List<String> getMerged(List<Path> paths) throws IOException {
+		List<String> mergedLines = new ArrayList<> ();
+		for (Path p : paths){
+			List<String> lines = Files.readAllLines(p, Charset.forName("UTF-8"));
+			if (!lines.isEmpty()) {
+				if (mergedLines.isEmpty()) {
+					mergedLines.add(lines.get(0)); //add header only once
+				}
+				mergedLines.addAll(lines.subList(1, lines.size()));
+			}
+		}
+		return mergedLines;
+	}
+
+	public MergeCSV() {
+		//super();
+		// TODO Auto-generated constructor stub
+	}
+	
+}
---
+++ src/Tests/ProjectTest.java
@@ -0,0 +1,100 @@
+package Tests;
+
+import static org.junit.Assert.*;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+
+public class ProjectTest {
+	KML.ConvertToKML junit = new KML.ConvertToKML();
+	CSV.ReadAndRewriteCSV junit2 = new CSV.ReadAndRewriteCSV();
+	CSV.MergeCSV junit3 = new CSV.MergeCSV();
+	KML.ConvertToKML junit4 = new KML.ConvertToKML();
+	Filter.FilterCSV junit5 = new Filter.FilterCSV();
+	
+	
+	@Test
+	public void returnModTest(){
+		String result =junit2.returnMod("c:/temp/file0.csv");
+		assertEquals("model=LG-H815", result);
+	}
+	
+	@Test
+	public void writeFileTest() throws IOException {
+		String expectedCSV="c:/temp2/NewC0.csv";
+		String orgCSV="c:/temp/file0.csv";
+		String testCSV="c:/temp2/testWriteFile.csv";
+		junit2.writeFile(orgCSV, testCSV);
+		String line;
+		BufferedReader expected = new BufferedReader(new FileReader(expectedCSV));
+		BufferedReader newCsv = new BufferedReader(new FileReader(testCSV));
+	    while ((line = expected.readLine()) != null) {
+	      assertEquals(line, newCsv.readLine());
+	    }
+		
+	}
+	
+	@Test
+	public void getMergedTest() throws IOException {
+		String expectedCSV="c:/temp2/merged.csv";
+		String orgCSV0="c:/temp2/NewC0.csv";
+		String orgCSV1="c:/temp2/NewC1.csv";
+		String testCSV="c:/temp2/testGetMerged.csv";
+		List<Path> paths=Arrays.asList(Paths.get(orgCSV0),Paths.get(orgCSV1));
+		List<String> mergedLines=junit3.getMerged(paths);
+		Files.write(Paths.get(testCSV), mergedLines);
+		
+		String line;
+		BufferedReader expected = new BufferedReader(new FileReader(expectedCSV));
+		BufferedReader newCsv = new BufferedReader(new FileReader(testCSV));
+	    while ((line = expected.readLine()) != null) {
+	      assertEquals(line, newCsv.readLine());
+	    }
+		
+	}
+	
+	@Test
+	public void csvToKmlTest() throws IOException {
+		String sourceCsv="c:/temp2/merged.csv";
+		String expectedKml="c:/temp2/final.kml";
+		File newKml=new File("c:/temp2/csvToKmlTest.kml");
+		junit4.setKmlLoc(newKml);
+		junit4.setCsvFile(sourceCsv);
+		junit4.csvToKml();
+		
+		String line;
+		BufferedReader expected = new BufferedReader(new FileReader(expectedKml));
+		BufferedReader newFile = new BufferedReader(new FileReader(newKml));
+	    while ((line = expected.readLine()) != null) {
+	      assertEquals(line, newFile.readLine());
+	    }
+	}
+	
+	@Test
+	public void filterTest() throws IOException {
+		String sourceCsv="c:/temp2/merged.csv";
+		String expectedCsv="c:/temp2/filterCSV.csv";
+		String testCSV="c:/temp2/TESTfilterCSV.csv";
+		junit5.setLastLocation(testCSV);
+		junit5.Filter();
+		
+		String line;
+		BufferedReader expected = new BufferedReader(new FileReader(expectedCsv));
+		BufferedReader newFile = new BufferedReader(new FileReader(testCSV));
+	    while ((line = expected.readLine()) != null) {
+	      assertEquals(line, newFile.readLine());
+	    }
+	}
+	
+
+}
