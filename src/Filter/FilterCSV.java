29edbb3065f0eef1a2d62d51ae84664653f5205e
Yarin Knafo <yarin665@gmail.com>
2017-12-19 12:45:07 +0200

    Update after timeFilter update

--- src/Filter/FilterCSV.java
+++ src/Filter/FilterCSV.java
@@ -4,6 +4,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.ParseException;
 import java.util.Scanner;
 
 public class FilterCSV {
@@ -27,8 +28,9 @@ public class FilterCSV {
 	 * @return
 	 * @throws FileNotFoundException
 	 * @throws IOException
+	 * @throws ParseException 
 	 */
-	public  String Filter() throws FileNotFoundException, IOException {
+	public  String Filter() throws FileNotFoundException, IOException, ParseException {
 		Scanner scanner = new Scanner( System.in );
 		
 		TimeFilter tf = new TimeFilter();
@@ -68,7 +70,7 @@ public class FilterCSV {
 		}
 
 
-
+		scanner.close();
 		return lastLocation;
 	}
 
@@ -193,18 +195,6 @@ public class FilterCSV {
 		this.lastLocation = lastLocation;
 	}
 	
-	public static double distFrom(double lat1, double lng1, double lat2, double lng2) {
-	    double earthRadius = 3958.75; // miles (or 6371.0 kilometers)
-	    double dLat = Math.toRadians(lat2-lat1);
-	    double dLng = Math.toRadians(lng2-lng1);
-	    double sindLat = Math.sin(dLat / 2);
-	    double sindLng = Math.sin(dLng / 2);
-	    double a = Math.pow(sindLat, 2) + Math.pow(sindLng, 2)
-	            * Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2));
-	    double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-	    double dist = earthRadius * c;
-
-	    return dist;
-	    }
+	
 	
 }
