029b922bcdbe3ae83052ec4ad2944c4b7ecf83d0
yakirarie <33618271+yakirarie@users.noreply.github.com>
2017-12-21 14:52:20 +0200

    Update MergeCSV.java
    Update merge file to a new function that get a folder

--- src/CSV/MergeCSV.java
+++ src/CSV/MergeCSV.java
@@ -1,39 +1,82 @@
 package CSV;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.PrintWriter;
+
 
 public class MergeCSV {
 
-	
 	/**
-	 * This function get all the new CSV (after converting them according to the format)
-	 * and merge them to one file
-	 * @param paths The list where all file locations are stored 
+	 * This function get all the new CSV (after converting them according to the
+	 * format) and merge them to one file
+	 * 
+	 * @param paths
+	 *            The list where all file locations are stored
 	 * @return The new String list containing all the info
 	 * @throws IOException
 	 */
-	public static List<String> getMerged(List<Path> paths) throws IOException {
-		List<String> mergedLines = new ArrayList<> ();
-		for (Path p : paths){
-			List<String> lines = Files.readAllLines(p, Charset.forName("UTF-8"));
-			if (!lines.isEmpty()) {
-				if (mergedLines.isEmpty()) {
-					mergedLines.add(lines.get(0)); //add header only once
-				}
-				mergedLines.addAll(lines.subList(1, lines.size()));
+
+
+	public static void GetMerged(File[] files) throws IOException {
+		PrintWriter pw = new PrintWriter(new FileOutputStream("c:/temp2/merged.csv"));
+
+		//Just for space
+		System.out.println();
+		
+		for (int i = 0; i < files.length; i++) {
+
+			System.out.println("Processing " + files[i].getPath() + "... ");
+			BufferedReader br = new BufferedReader(new FileReader(files[i].getPath()));
+			
+			//If it's not the first file, skip the first line
+			if (i > 0)
+				br.readLine();
+			
+			String line = br.readLine();
+			while (line != null) {
+				pw.println(line);
+				line = br.readLine();
 			}
+			br.close();
 		}
-		return mergedLines;
+		pw.close();
+		System.out.println("All files have been concatenated into merged.csv\n");
+	}
+	
+	
+	public static void GetMerged(File[] files, String mergeLoc) throws IOException {
+		PrintWriter pw = new PrintWriter(new FileOutputStream(mergeLoc));
+
+		//Just for space
+		System.out.println();
+		
+		for (int i = 0; i < files.length; i++) {
+
+			System.out.println("Processing " + files[i].getPath() + "... ");
+			BufferedReader br = new BufferedReader(new FileReader(files[i].getPath()));
+			
+			//If it's not the first file, skip the first line
+			if (i > 0)
+				br.readLine();
+			
+			String line = br.readLine();
+			while (line != null) {
+				pw.println(line);
+				line = br.readLine();
+			}
+			br.close();
+		}
+		pw.close();
+		System.out.println("All files have been concatenated into merged.csv\n");
 	}
 
 	public MergeCSV() {
-		//super();
+		// super();
 		// TODO Auto-generated constructor stub
 	}
-	
+
 }
