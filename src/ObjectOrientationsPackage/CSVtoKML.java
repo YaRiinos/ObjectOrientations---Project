af40b40df364bab2a7fbbc0527ec0b0e510d208c
yakirarie <33618271+yakirarie@users.noreply.github.com>
2017-12-21 14:53:35 +0200

    Update CSVtoKML.java
    Update main according to the new merge function

--- src/ObjectOrientationsPackage/CSVtoKML.java
+++ src/ObjectOrientationsPackage/CSVtoKML.java
@@ -1,12 +1,8 @@
 package ObjectOrientationsPackage;
+
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.text.ParseException;
-import java.util.Arrays;
-import java.util.List;
 
 import Algorithms.Algo;
 import CSV.MergeCSV;
@@ -14,77 +10,61 @@ import CSV.ReadAndRewriteCSV;
 import Filter.FilterCSV;
 import KML.ConvertToKML;
 
-
-
-
 /**
- * Converting, sorting and filtering a CSV file to a KML file 
+ * Converting, sorting and filtering a CSV file to a KML file
+ * 
  * @author Yarin
  * 
- * @see
- * <b>CSV:</b> https://en.wikipedia.org/wiki/Comma-separated_values <P>
- * <b>KML:</b> https://en.wikipedia.org/wiki/Keyhole_Markup_Language
+ * @see <b>CSV:</b> https://en.wikipedia.org/wiki/Comma-separated_values
+ *      <P>
+ *      <b>KML:</b> https://en.wikipedia.org/wiki/Keyhole_Markup_Language
  *
- *@version 1.0
+ * @version 1.0
  */
-
-
 
 public class CSVtoKML {
 
-	
-	public static void main(String[] args) throws IOException, ParseException{
+	public static void main(String[] args) throws IOException, ParseException {
 
-		//Get the list of file in the 'temp' folder and changing them to the format
+		// Get the list of file in the 'temp' folder and changing them to the format
 		File[] fileList = new File("c:/temp").listFiles();
 
-		for (int i=0;i < fileList.length;i++) {
-			String newCsv="c:/temp2/NewC"+i+".csv";
-			ReadAndRewriteCSV MakeCSV=new ReadAndRewriteCSV("c:/temp/file"+i+".csv");
+		for (int i = 0; i < fileList.length; i++) {
+			String newCsv = "c:/temp2/afterFormat/NewC" + i + ".csv";
+			ReadAndRewriteCSV MakeCSV = new ReadAndRewriteCSV("c:/temp/file" + i + ".csv");
 			ReadAndRewriteCSV.writeFile(MakeCSV.getCsvFile(), newCsv);
 		}
 
-		
-		//Get the new CSV files and merge them to one file
-		//MergeCSV mergeCSVOp = null;
-		List<Path> paths = Arrays.asList(Paths.get("c:/temp2/NewC0.csv"),Paths.get("c:/temp2/NewC1.csv"));
-		List<String> mergedLines = MergeCSV.getMerged(paths);
-		Path target = Paths.get("c:/temp2/merged.csv");
-		Files.write(target, mergedLines);
+		// Get the new CSV files and merge them to one file
+		File file = new File("c:/temp2/afterFormat/");
+		File[] files = file.listFiles();
 
-		String filterCSV="c:/temp2/filterCSV.csv";
+		MergeCSV.GetMerged(files);
 
-		//Use filter function [Input, Output]
-		//Filter(mergeCSV, filterCSV);
-		FilterCSV FilterObj=new FilterCSV();
+		String filterCSV = "c:/temp2/filterCSV.csv";
+
+		// Use filter function [Input, Output]
+		FilterCSV FilterObj = new FilterCSV();
 		FilterObj.Filter();
-		
-		
-		
 
-		//Check if filter CSV is exist, if so convert him to KML
-		//else convert the merge CSV to KML
-		ConvertToKML Converter=new ConvertToKML();
+		// Check if filter CSV is exist, if so convert him to KML
+		// else convert the merge CSV to KML
+		ConvertToKML Converter = new ConvertToKML();
 		File f = new File(filterCSV);
-		if(f.exists() && !f.isDirectory()) { 
+		if (f.exists() && !f.isDirectory()) {
 			Converter.setCsvFile(filterCSV);
 			Converter.csvToKml();
-		}
-		else
+		} else
 			Converter.csvToKml();
 
 		System.out.println("Done\n");
-		
-		Algo a=new Algo();
-		//a.firstAlgoAllMACs();
 
-		//Algo2 x=new Algo2();
-	//	x.secondAlgo();
+		//Algo a = new Algo();
+		// a.firstAlgoAllMACs();
 
-	}//END MAIN
+		// Algo2 x=new Algo2();
+		// x.secondAlgo();
 
-	
+	}// END MAIN
 
-	
-
-}//END PROGRAM
+}// END PROGRAM
