236a50223f4bd2a6f56abbf09e3a28ceb2b25794
Yarin Knafo <yarin665@gmail.com>
2017-12-23 21:13:51 +0200

    Update for not exit program when exit win

--- src/GUI/helpWin.java
+++ src/GUI/helpWin.java
@@ -3,12 +3,12 @@ package GUI;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
 
 public class helpWin {
 
 	private JFrame frame;
-	private JTextField txtHelpWind;
 
 	/**
 	 * Launch the application.
@@ -38,14 +38,13 @@ public class helpWin {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 594, 377);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setBounds(100, 100, 765, 504);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		txtHelpWind = new JTextField();
-		txtHelpWind.setText("Help wind");
-		txtHelpWind.setBounds(118, 76, 86, 20);
-		frame.getContentPane().add(txtHelpWind);
-		txtHelpWind.setColumns(10);
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setIcon(new ImageIcon(helpWin.class.getResource("/help2.png")));
+		lblNewLabel.setBounds(0, 0, 749, 465);
+		frame.getContentPane().add(lblNewLabel);
 	}
 }
