a490741cbcaec0dfeb619614fa693044c0333387
Yarin Knafo <yarin665@gmail.com>
2018-01-02 12:11:45 +0200

    add explain and load files after format

--- src/GUI/openFile.java
+++ src/GUI/openFile.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.ArrayList;
+
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 
@@ -30,6 +31,7 @@ public class openFile {
 	// Add one file to the database
 	public void pickFile(ArrayList<String> database) throws Exception {
 
+		//Set the dir to C
 		fileChooser.setCurrentDirectory(new File("C:\\"));
 
 		if (fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
@@ -37,35 +39,68 @@ public class openFile {
 			// get the file
 			java.io.File file = fileChooser.getSelectedFile();
 
-			ReadAndRewriteCSV.writeFile(file.toString(), tempFile);
+			//Check if a file is before a format or after
+			BufferedReader bf = new BufferedReader(new FileReader(file));
+			String checkFile=bf.readLine();
+			
+			//Read the first word, if it's like the format continue, else make it like the format.
+			String [] colS= checkFile.split(",");
+			if(colS[0].equals("Time"))
+				tempFile=file.getAbsolutePath();
+				
+			else//Use format on the file
+				ReadAndRewriteCSV.writeFile(file.toString(), tempFile);
 
+			bf.close();
+			
 			try (BufferedReader br = new BufferedReader(new FileReader(tempFile))) {
-
+				
+				//Read the first line
 				br.readLine();
 
-				while ((line = br.readLine()) != null)
+				while ((line = br.readLine()) != null) 
 					database.add(line);
+				
+					
 
 			}
 
 		} else {
 			sb.append("No file was selected");
 		}
+		
 	}
 
 	public void pickFolder(ArrayList<String> database) throws Exception {
 
+		//Set the dir to C
 		fileChooser.setCurrentDirectory(new File("C:\\"));
+		
+		//Set the choosing option to be just directories
 		fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 
 		if (fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
 
+			//Add all the files from the folder to array
 			File theFolder = fileChooser.getSelectedFile();
 			File[] files = theFolder.listFiles();
 
 			for (int i = 0; i < files.length; i++) {
 
-				ReadAndRewriteCSV.writeFile(files[i].toString(), tempFile);
+				//Check if a file is before a format or after
+				BufferedReader bf = new BufferedReader(new FileReader(files[i]));
+				String checkFile=bf.readLine();
+				
+				//Read the first word, if it's like the format continue, else make it like the format.
+				String [] colS= checkFile.split(",");
+				if(colS[0].equals("Time"))
+					tempFile=files[i].getAbsolutePath();
+					
+				else//Use format on file
+					ReadAndRewriteCSV.writeFile(files[i].toString(), tempFile);
+
+				bf.close();
+				
 
 				try (BufferedReader br = new BufferedReader(new FileReader(tempFile))) {
 
@@ -96,18 +131,22 @@ public class openFile {
 				+ " SSID10, MAC10, Frequncy10, Signal10";
 
 		JFileChooser chooser = new JFileChooser();
+		//Set the dir to C
 		chooser.setCurrentDirectory(new File("C:\\temp2"));
+		
 		int retrival = chooser.showSaveDialog(null);
 		if (retrival == JFileChooser.APPROVE_OPTION) {
 			try (FileWriter fw = new FileWriter(chooser.getSelectedFile() + ".csv")) {
 
+				//Write the header once
 				fw.write(FILE_HEADER.toString());
 				fw.write(NEW_LINE_SEPARATOR.toString());
 
+				//Write all the database to the file
 				for (int i = 0; i < database.size(); i++) {
 					fw.write(database.get(i).toString());
 					fw.write(NEW_LINE_SEPARATOR.toString());
-					i++;
+					
 				}
 
 				fw.close();
@@ -115,6 +154,7 @@ public class openFile {
 				ex.printStackTrace();
 			}
 
+			//Show a win that the file has been created
 			JOptionPane.showMessageDialog(null, "CSV has been saved", "File Saved", JOptionPane.INFORMATION_MESSAGE);
 		}
 
@@ -133,18 +173,21 @@ public class openFile {
 				+ " SSID10, MAC10, Frequncy10, Signal10";
 
 		JFileChooser chooser = new JFileChooser();
+		//Set the dir to C
 		chooser.setCurrentDirectory(new File("C:\\temp2"));
+		
 		int retrival = chooser.showSaveDialog(null);
+		
 		if (retrival == JFileChooser.APPROVE_OPTION) {
 			try (FileWriter fw = new FileWriter(chooser.getSelectedFile() + ".csv")) {
 
+				//Get all the database to a CSV file
 				fw.write(FILE_HEADER.toString());
 				fw.write(NEW_LINE_SEPARATOR.toString());
 
 				for (int i = 0; i < database.size(); i++) {
 					fw.write(database.get(i).toString());
 					fw.write(NEW_LINE_SEPARATOR.toString());
-					i++;
 				}
 
 				fw.close();
@@ -155,12 +198,15 @@ public class openFile {
 
 			File kmlLoc = new File(chooser.getSelectedFile() + ".kml");
 
+			//Convert the CSV we created to KML
 			ConvertToKML converter = new ConvertToKML(chooser.getSelectedFile() + ".csv", kmlLoc);
 			converter.csvToKml();
 
+			//Delete the CSV file
 			File csvDelete = new File(chooser.getSelectedFile() + ".csv");
 			csvDelete.delete();
 
+			//Show a window that the KML has been created 
 			JOptionPane.showMessageDialog(null, "KML has been saved", "File Saved", JOptionPane.INFORMATION_MESSAGE);
 		}
 
